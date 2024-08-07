6ffbd49fb68e27a3e00bbbf28d565a6b52afe7b9
Mohamad Mgar <shwaeki@gmail.com>
2024-08-07 11:51:14 +0300

    update

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -162,7 +162,7 @@
                         </div>
                         <div class="chat-footer chat-active">
                             <div class="chat-input">
-                                <form class="chat-form" id="chat-form" wire:submit.prevent="sendMessage">
+                                <form class="chat-form" id="chat-form" wire:submit.prevent="sendStudentMessage">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                          viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                          stroke-linecap="round" stroke-linejoin="round"
