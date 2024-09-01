77462ec991c1736cbcfdf6fb3ae68d2222d71bb1
Shwaeki@WithYou <shwaeki98@gmail.com>
2024-09-01 17:07:41 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -733,7 +733,6 @@
 
                     @foreach(auth()->user()->supervisorYearClasses()->where('academic_year_id',$activeAcademicYear->id)->get() ?? [] as $yearClass)
                         @if($yearClass->schoolClass && $yearClass->schoolClass->archived == false)
-
                             <li class="menu">
                                 <a href="{{route('school-classes.show', $yearClass->schoolClass)  }}"
                                    aria-expanded="{{ Route::is('school-classes.show', $yearClass->schoolClass) ? 'true' : 'false' }}"
@@ -760,6 +759,40 @@
                             </li>
                         @endif
                     @endforeach
+
+
+                        <li class="menu menu-heading">
+                            <div class="heading"><span>  فصولي الدراسية للمساعدة</span></div>
+                        </li>
+
+                        @foreach(auth()->user()->supervisorYearClasses()->where('academic_year_id',$activeAcademicYear->id)->get() ?? [] as $yearClass)
+                            @if($yearClass->schoolClass && $yearClass->schoolClass->archived == false)
+                                <li class="menu">
+                                    <a href="{{route('school-classes.show', $yearClass->schoolClass)  }}"
+                                       aria-expanded="{{ Route::is('school-classes.show', $yearClass->schoolClass) ? 'true' : 'false' }}"
+                                       class="dropdown-toggle">
+                                        <div class="">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                 viewBox="0 0 24 24"
+                                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                 stroke-linejoin="round" class="feather feather-cpu">
+                                                <rect x="4" y="4" width="16" height="16" rx="2" ry="2"></rect>
+                                                <rect x="9" y="9" width="6" height="6"></rect>
+                                                <line x1="9" y1="1" x2="9" y2="4"></line>
+                                                <line x1="15" y1="1" x2="15" y2="4"></line>
+                                                <line x1="9" y1="20" x2="9" y2="23"></line>
+                                                <line x1="15" y1="20" x2="15" y2="23"></line>
+                                                <line x1="20" y1="9" x2="23" y2="9"></line>
+                                                <line x1="20" y1="14" x2="23" y2="14"></line>
+                                                <line x1="1" y1="9" x2="4" y2="9"></line>
+                                                <line x1="1" y1="14" x2="4" y2="14"></line>
+                                            </svg>
+                                            <span>{{$yearClass->schoolClass?->name}}</span>
+                                        </div>
+                                    </a>
+                                </li>
+                            @endif
+                        @endforeach
 
                 @endauth
 
