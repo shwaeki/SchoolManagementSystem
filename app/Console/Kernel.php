aac635b7683151edbe90293bed2bafdc746ae6ab
Mohamad Shwaeki <shwaeki@gmail.com>
2024-10-21 18:44:39 +0300

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command("queue:work --daemon")->everyMinute()->withoutOverlapping();
+      //  $schedule->command("queue:work --daemon")->everyMinute()->withoutOverlapping();
         $schedule->job(new CheckTeacherAttendance())->dailyAt("09:00")->withoutOverlapping();
     }
 
